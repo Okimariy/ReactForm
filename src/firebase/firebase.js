a97244ec0171f5363f3fcf3843a26328914925d8
Okimariy <okimari1002@icloud.com>
2020-10-03 17:03:17 +0900

    atom変更

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,15 @@
 import firebase from "firebase";
 import "firebase/auth";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  apiKey: "AIzaSyDKfDCsQ6IYksJUksRB1MOw4JBCLpqxuqI",
+  authDomain: "companyform-practice.firebaseapp.com",
+  databaseURL: "https://companyform-practice.firebaseio.com",
+  projectId: "companyform-practice",
+  storageBucket: "companyform-practice.appspot.com",
+  messagingSenderId: "821017918488",
+  appId: "1:821017918488:web:010b3aa58b979c2876576d",
+};
 firebase.initializeApp(firebaseConfig);
 export default firebaseConfig;
 // export const functions = firebase.functions();
